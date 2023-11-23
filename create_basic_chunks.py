fa38ad0ec7b53dc9c7fc27c450ef5917a0a11b34
modebralski <m.odebralski@gmail.com>
2023-11-23 23:03:18 +0100

    created create_black_chunk()

--- create_basic_chunks.py
+++ create_basic_chunks.py
@@ -3,12 +3,19 @@ from PIL import Image
 
 def main():
     create_white_chunk()
+    create_black_chunk()
 
 
 def create_white_chunk():
-    image_0 = Image.new('RGBA', (2, 2), (255, 255, 255, 255))
-    image_0.save('0.png')
-    image_0.show()
+    white_chunk = Image.new('RGBA', (2, 2), (255, 255, 255, 255))
+    white_chunk.save('0.png')
+    white_chunk.show()
+
+
+def create_black_chunk():
+    black_chunk = Image.new('RGBA', (2, 2), (0, 0, 0, 255))
+    black_chunk.save('1.png')
+    black_chunk.show()
 
 
 if __name__ == "__main__":
