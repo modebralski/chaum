2cf9ea9e0351c40eafb0ecb597e80107e4951a9d
modebralski <m.odebralski@gmail.com>
2023-11-24 00:01:16 +0100

    creates .png based on array

--- create_basic_chunks.py
+++ create_basic_chunks.py
@@ -9,30 +9,30 @@ def main():
 
 
 def create_white_chunk():
-    white_chunk = Image.new('RGBA', (2, 2), (255, 255, 255, 255))
-    white_chunk.save('0.png')
+    white_chunk = Image.new("RGBA", (2, 2), (255, 255, 255, 255))
+    white_chunk.save("0.png")
     white_chunk.show()
 
 
 def create_black_chunk():
-    black_chunk = Image.new('RGBA', (2, 2), (0, 0, 0, 255))
-    black_chunk.save('1.png')
+    black_chunk = Image.new("RGBA", (2, 2), (0, 0, 0, 255))
+    black_chunk.save("1.png")
     black_chunk.show()
 
 
 def create_semi_black_2_chunk():
-    semi_black_2_chunk = Image.new('RGBA', (2, 2), (0, 0, 0, 0))
+    semi_black_2_chunk = Image.new("RGBA", (2, 2), (0, 0, 0, 0))
     semi_black_2_chunk.putpixel((0, 1), (0, 0, 0, 255))
     semi_black_2_chunk.putpixel((1, 0), (0, 0, 0, 255))
-    semi_black_2_chunk.save('2.png')
+    semi_black_2_chunk.save("2.png")
     semi_black_2_chunk.show()
 
 
 def create_semi_black_3_chunk():
-    semi_black_3_chunk = Image.new('RGBA', (2, 2), (0, 0, 0, 0))
+    semi_black_3_chunk = Image.new("RGBA", (2, 2), (0, 0, 0, 0))
     semi_black_3_chunk.putpixel((0, 0), (0, 0, 0, 255))
     semi_black_3_chunk.putpixel((1, 1), (0, 0, 0, 255))
-    semi_black_3_chunk.save('3.png')
+    semi_black_3_chunk.save("3.png")
     semi_black_3_chunk.show()
 
 
--- main.py
+++ main.py
@@ -5,28 +5,26 @@ from PIL import Image
 def main():
     size_x = 10
     size_y = 10
-    img = create_img_matrix(size_x, size_y)
-    top_layer = create_top_random_layer(size_x, size_y)
-    bottom_layer = create_bottom_layer(img, top_layer)
-    print(f"Image:\n {img}")
-    print(f"Random Sheet: \n {top_layer}")
-    print(f"Other Sheet: \n {bottom_layer}")
-    images = [Image.open(f"{i}.bmp") for i in range(4)]
-    original_image = Image.new("RGB", (10, 10))
-    for row in range(10):
-        for col in range(10):
-            value = img[row][col]
-            color = images[value].getpixel((0, 0))
-            original_image.putpixel((col, row), color)
-    original_image.save("original.bmp")
-
-    random_image = Image.new("RGB", (10, 10))
-    for row in range(10):
-        for col in range(10):
-            value = top_layer[row][col]
-            color = images[value].getpixel((0, 0))
-            random_image.putpixel((col, row), color)
-    random_image.save("random.bmp")
+    img_array = create_img_matrix(size_x, size_y)
+    top_layer_array = create_top_random_layer(size_x, size_y)
+    bottom_layer_array = create_bottom_layer(img_array, top_layer_array)
+    print(f"Image:\n {img_array}")
+    print(f"Random Sheet: \n {top_layer_array}")
+    print(f"Other Sheet: \n {bottom_layer_array}")
+    zero_image = Image.open('0.png')
+    one_image = Image.open('1.png')
+    two_image = Image.open('2.png')
+    three_image = Image.open('3.png')
+    img_size = img_array.shape[0] * 2, img_array.shape[1] * 2
+    img = Image.new('RGBA', img_size, (0, 0, 0, 0))
+    for row in range(img_array.shape[0]):
+        for column in range(img_array.shape[1]):
+            if img_array[row, column] == 0:
+                img.paste(zero_image, (column * 2, row * 2))
+            else:
+                img.paste(one_image, (column * 2, row * 2))
+    img.save("img.png")
+    img.show()
 
 
 def create_img_matrix(size_x: int, size_y: int):
